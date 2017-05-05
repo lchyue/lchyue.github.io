dbba05567b1ceb1ac1443f724e03d0f38ca6c7a1
yue.huang <yue.huang@lincomb.com>
2017-05-05 17:12:26 +0800

    aa

--- js/lincombMoney.js
+++ js/lincombMoney.js
@@ -1,6 +1,4 @@
-
-
-  touchClick($('.kinds li'),function(){
+touchClick($('.kinds li'),function(){
    $('.kinds li div').addClass('hide');
      $('.content .moneyContent').addClass('hide');
    $('.kinds li div').eq($(this).index()).removeClass('hide');
@@ -9,65 +7,45 @@
   /*  console.log($(this).index())*/
      $('.content .moneyContent').eq($(this).index()).removeClass('hide');
  });
- $('.nav').on('touchstart',drag);
- /*$('.kinds li').on('touchstart',drag);*/
+ $('.nav-content').on('touchstart',drag);
+ $('.kinds li').on('touchstart',drag);
  function drag(ev){
     var moveType = '';
      var disY = ev.originalEvent.touches[0].pageY;
      // console.log(disY);
-    $('.nav').on('touchmove',touchMove);
-   $('.nav').on('touchend',touchEnd);
+    $(document).on('touchmove',touchMove);
+   $(document).on('touchend',touchEnd);
   function touchMove(ev){
      if(disY-ev.originalEvent.touches[0].pageY > 50){
         moveType = 'top';
      }
       else if(disY-ev.originalEvent.touches[0].pageY < -50){
-         
           moveType = 'down';
-          
        }
      }
    function touchEnd(){
          //end
         $(document).off('touchmove',touchMove);
-        $('.nav').off('touchend',touchEnd);
+        $(document).off('touchend',touchEnd);
         switch(moveType){
           case 'top':
-              $('body').css('overflow','auto');
-              $('.nav-content').slideUp(550);
+                $('.nav-content').slideUp(550);
               $('.nav').animate( { height: "1.1rem" }, 500 );
-              /*$(".kinds").css({
-                  "position":"fixed",
-                  "top":0,
-                  "height":"1.1rem",
-                  "width":"100%",
-                  "background":"-webkit-gradient(linear, 0 0, 0 100%, from(#20bbed),to(#23b2f0))"
-            });*/
-
                break;
            case 'down':
-           /*     alert();*/
-               $('body').css('overflow','auto');
-               $('.nav-content').slideDown(300);
+                 $('.nav-content').slideDown(300);
                $('.nav').animate( { height: "6.1rem" }, 500 );
-               /*$(".kinds").css({
-                  "position":"static",
-                  "background":"rgba(255,255,255,0)",
-                  height:"auto"
-              });*/
-
                break;
          }
      }
      ev.originalEvent.preventDefault();
  }
-
  function touchClick(obj,fn){   
    obj.on('touchstart',function(ev){
        var _this = this;
        var moveCheck = false;
-       $('.nav').on('touchmove',touchMove);
-       $('.nav').on('touchend',touchEnd);
+       $(document).on('touchmove',touchMove);
+       $(document).on('touchend',touchEnd);
        function touchMove(){
            moveCheck = true;
        }
@@ -75,10 +53,10 @@
            if(!moveCheck){
                fn && fn.call(_this);
            }
-          $('.nav').off('touchmove',touchMove);
-          $('.nav').off('touchend',touchEnd);
+          $(document).off('touchmove',touchMove);
+          $(document).off('touchend',touchEnd);
       }
-     /*   ev.originalEvent.stopPropagation();*/
-       ev.originalEvent.preventDefault();
+        ev.originalEvent.stopPropagation();
+      ev.isDefaultPrevented();
   });
  }  
